303a308fa8e016e6e0f07d2fc55d07a4215ea596
Ananda Rahayu <134572675+anandarahayu22@users.noreply.github.com>
2024-03-19 20:18:33 +0700

    Add files via upload

---
+++ ttc_latihan2.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main() {
+  //Fahrenheit To Celcius
+  stdout.write("Masukkan suhu dalam Fahrenheit: ");
+  var fahrenheit = double.parse(stdin.readLineSync()!);
+  var fahrenheitToCelcius = ((fahrenheit - 32) * 5 / 9).toStringAsFixed(2);
+  print(
+      "$fahrenheit derajat Fahrenheit = $fahrenheitToCelcius derajat Celcius");
+
+  //Reamur To Celcius
+  stdout.write("Masukkan suhu dalam Reamur: ");
+  var reamur = double.parse(stdin.readLineSync()!);
+  var reamurToCelcius = ((5 / 4) * reamur).toStringAsFixed(2);
+  print("$reamur derajat Reamur = $reamurToCelcius derajat Celcius");
+
+  //Kelvin To Celcius
+  stdout.write("Masukkan suhu dalam Kelvin: ");
+  var kelvin = double.parse(stdin.readLineSync()!);
+  var kelvinToCelcius = (kelvin - 273.15).toStringAsFixed(2);
+  print("$kelvin derajat Kelvin = $kelvinToCelcius derajat Celcius");
+}
